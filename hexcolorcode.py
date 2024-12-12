4dd32bd0d4aef1066789b464213b4f1403abbdff
Viinnyyyy <akinlabi.aderibigbe@stu.cu.edu.ng>
2024-12-12 19:03:51 +0100

    Completed the regex issue. Ongoing input loop

--- hexcolorcode.py
+++ hexcolorcode.py
@@ -1,18 +1,13 @@
 import re
-code_lines = []
-while True:
-    code_line = input()
-    if code_line:
-        code_lines.append(code_line)
-    else:
-        break
-# line_string = user_string.split('\n')
-code_length = int(code_lines[0])
-validating_list = []
-hex_detector = r"#[a-fA-F0-9]{3}|#[a-fA-F0-9]{6}"
-for line in code_lines[1:]:
-    validator = re.findall(hex_detector, line)
-    validating_list.append(*validator)
+user_input = int(input())
+validating_list = ''
+hex_detector = r".#[a-fA-F0-9]{6}|.#[a-fA-F0-9]{3}"
+for i in range(user_input):
+    code_line = str(input())
+    print(code_line)
+    validator = re.findall(hex_detector, code_line)
+    # print(validator)
+    validating_list = validating_list + ('\n'.join(validator))
 # print('\n'.join(validating_list))
-print(*validating_list, sep='\n')
+print(validating_list, sep='\n')
 
